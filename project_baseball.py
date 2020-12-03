d8b2070702385a583a890cc694a18ad420070b3c
Nader Esmael <ncesmael@gmail.com>
2020-12-02 16:08:42 -0800

    add cleaning function

--- project_baseball.py
+++ project_baseball.py
@@ -1,6 +1,15 @@
 """
 Functions contained in here will help with the modeling process
 """
+# import numpy as np
+# import pandas as pd
+features = ['game_date','pitch_type','Pitcher_name', 'batter_id','pitcher_id','release_speed', 'release_pos_x', 'release_pos_z', 'stand', 'p_throws', 'balls', 'strikes',
+            'pfx_x', 'pfx_z', 'plate_x', 'plate_z', 'on_3b', 'on_2b', 'on_1b', 'inning', 'inning_topbot',
+            'effective_speed', 'release_spin_rate', 'release_extension','Catcher','FirstBasemen', 'SecondBasemen', 'ThirdBasemen', 'ShortStop',
+            'LeftField', 'CenterField', 'RightField', 'at_bat_number', 'pitch_number', 'pitch_name',
+            'bat_score', 'fld_score', 'post_bat_score', 'post_fld_score', 'if_fielding_alignment', 'of_fielding_alignment',
+            'count', 'strike_attempt','events','description','type','bb_type','vx0','vy0','vz0',
+            'ax','ay','az','outs_when_up']
 
 def pitcher_stats(name ,df):
     '''
@@ -49,10 +58,50 @@ def dropping_columns(mlb):
     return mlb
 
 ### Feature Engineering Functions ###
-def batter_name(des):
-    try:
-        name = ' '.join(des.split(' ',2)[:2])
-        return name
-    except:
-        return np.nan
 
+
+### Cleaning Function Totality ###
+
+def cleaning(df):
+    df = renaming_fielders(df)
+    df = dropping_columns(df)
+    
+    df['game_date']=pd.to_datetime(df['game_date'])
+    df['pitch_type']=df['pitch_type'].map({'CS':'CU',
+                           'FF':'FF',
+                           'CU':'CU',
+                           'CH':'CH',
+                           'SI':'SI',
+                           'SL':'SL',
+                           'FC':'FC',
+                           'KC':'KC',
+                           'FS':'FS',
+                           'KN':'KN',
+                           'FO':'FO'})
+    df = df[df['release_speed'].notnull()]
+    
+    df['pitch_type'].fillna('U', inplace=True)
+    df['pitch_name'].fillna('Unknown', inplace=True)
+    df['if_fielding_alignment'].fillna('Unknown', inplace=True)
+    df['of_fielding_alignment'].fillna('Unknown', inplace=True)
+    
+    return df
+
+def strike_attempt_column(df):
+    
+    df['strike_attempt']=df['description'].map(
+            {'called_strike':'strike',
+             'swinging_strike':'strike',
+             'ball':'ball',
+             'foul':'strike', # False in out_via_description
+             'hit_into_play':'out',
+             'blocked_ball':'ball',
+             'hit_into_play_score':'ob',
+             'swinging_strike_blocked':'strike',
+             'hit_into_play_no_out':'ob',
+             'foul_bunt':'strike',
+             'foul_tip':'strike',
+             'hit_by_pitch':'ob',
+             'missed_bunt':'strike',
+             'pitchout':'out',
+             'bunt_foul_tip':'strike'})
