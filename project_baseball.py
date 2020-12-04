dc278c4bda13d9d27468fdad7977eba4fed16a25
Nader Esmael <ncesmael@gmail.com>
2020-12-03 22:28:06 -0800

    Done cleaning, added func

--- project_baseball.py
+++ project_baseball.py
@@ -4,6 +4,7 @@ Functions contained in here will help with the modeling process
 import numpy as np
 import pandas as pd
 
+# Global Variables
 features = ['game_date','pitch_type','Pitcher_name', 'batter_id','pitcher_id','release_speed', 'release_pos_x', 'release_pos_z', 'stand', 'p_throws', 'balls', 'strikes',
             'pfx_x', 'pfx_z', 'plate_x', 'plate_z', 'on_3b', 'on_2b', 'on_1b', 'inning', 'inning_topbot',
             'effective_speed', 'release_spin_rate', 'release_extension','Catcher','FirstBasemen', 'SecondBasemen', 'ThirdBasemen', 'ShortStop',
@@ -11,6 +12,23 @@ features = ['game_date','pitch_type','Pitcher_name', 'batter_id','pitcher_id','r
             'bat_score', 'fld_score', 'post_bat_score', 'post_fld_score', 'if_fielding_alignment', 'of_fielding_alignment',
             'count', 'strike_attempt','events','description','type','bb_type','vx0','vy0','vz0',
             'ax','ay','az','outs_when_up']
+
+ids = ['pitcher_id','batter_id','on_1b','on_2b','on_3b','Catcher','FirstBasemen',
+      'SecondBasemen', 'ThirdBasemen', 'ShortStop', 'LeftField','CenterField', 'RightField']
+make_binary = ['stand','p_throws']
+integerify = ['outs_when_up','bat_score','fld_score','post_bat_score','post_fld_score','pitch_number',
+             'at_bat_number','inning','balls','strikes']
+f2 = ['game_date', 'pitch_type', 'Pitcher_name', 'pitcher_id', 'batter_name',
+       'batter_id', 'release_speed', 'release_pos_x', 'release_pos_z', 'stand',
+       'p_throws', 'balls', 'strikes', 'pfx_x', 'pfx_z', 'plate_x', 'plate_z',
+       'on_3b', 'on_2b', 'on_1b', 'inning', 'inning_topbot', 'effective_speed',
+       'release_spin_rate', 'release_extension', 'Catcher', 'FirstBasemen',
+       'SecondBasemen', 'ThirdBasemen', 'ShortStop', 'LeftField',
+       'CenterField', 'RightField', 'at_bat_number', 'pitch_number',
+       'pitch_name', 'bat_score', 'fld_score', 'post_bat_score',
+       'post_fld_score', 'if_fielding_alignment', 'of_fielding_alignment',
+       'count', 'strike_attempt', 'events', 'description', 'type', 'bb_type',
+       'vx0', 'vy0', 'vz0', 'ax', 'ay', 'az', 'outs_when_up']
 
 def pitcher_stats(name ,df):
     '''
@@ -128,3 +146,21 @@ def the_count(df):
     
     df['count'] = df['balls'].map(int).map(str) +"-"+ df['strikes'].map(int).map(str)
     return df
+
+def more_cleaning(df):
+    # Change game_date dtype
+    df['game_date']=pd.to_datetime(df['game_date'])
+    # Apply a fillna to the bases with 0s and turn them into ints to get IDs
+    df[ids] = df[ids].fillna(0)
+    for i in ids:
+        df[i] = df[i].map(int)
+    # Binary columns
+    df = pd.get_dummies(df, columns=make_binary, drop_first=True)
+    df.rename(columns={'stand_R':'stand', 'p_throws_R':'p_throws'},inplace=True)
+    df = pd.get_dummies(df, columns=['inning_topbot'], drop_first=True)
+    df.rename(columns={'inning_topbot_Top':'inning_topbot'},inplace=True)
+    # Int Conversion
+    for i in integerify:
+        df[i] = df[i].map(int)
+    
+    return df[f2]
